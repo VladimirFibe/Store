e9a49438ae53a4e72c6d9c8d1657024400c752fd
Vladimir Fibe <motiw@icloud.com>
2024-05-20 18:44:34 +0500

    Initial Commit

---
+++ Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Store
+//
+//  Created by MacService on 20.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
